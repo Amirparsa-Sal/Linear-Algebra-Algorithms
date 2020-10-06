f146db98ddc14132e7183c364e71e26f9c03daa0
Amirparsa-Sal <Amirparsa.s@aut.ac.ir>
2020-10-07 02:47:06 +0330

    complete show result

--- Row Reduction/row_reduction.py
+++ Row Reduction/row_reduction.py
@@ -46,44 +46,66 @@ class RowReductionHandler():
 	
 	def do_forward_phase(self, starting_row=0, starting_col=0):
 		pivot_col = self.find_pivot_column(starting_row,starting_col)
-		self.pivot_positions.append((starting_row,pivot_col))
-		if starting_row == self.rows_num-1 or pivot_col == self.cols_num-1:
-			return
-		max_index = self.find_absolute_max_in_column(pivot_col,starting_row)
-		self.interchange_row(starting_row,max_index)
-		print(self.aug_matrix)
-		for i in range(starting_row+1,self.rows_num):
-			self.replacement_row(i,starting_row,pivot_col)
-		self.do_forward_phase(starting_row+1,starting_col+1)
+		if pivot_col!=-1:
+			self.pivot_positions.append((starting_row,pivot_col))
+			if starting_row == self.rows_num-1 or pivot_col == self.cols_num-1:
+				return
+			max_index = self.find_absolute_max_in_column(pivot_col,starting_row)
+			self.interchange_row(starting_row,max_index)
+			for i in range(starting_row+1,self.rows_num):
+				self.replacement_row(i,starting_row,pivot_col)
+			self.do_forward_phase(starting_row+1,starting_col+1)
 
 	def do_backward_phase(self):
 		for i in range(len(self.pivot_positions)-1,-1,-1):
 			pivot = self.pivot_positions[i]
 			row = pivot[0]
 			col = pivot[1]
-			for i in range(row-1,-1,-1):
-				self.replacement_row(i,row,col)
 			num = self.aug_matrix[row,col]
 			if num!=1:
 				self.scale_row(row,1/num)
+			for i in range(row-1,-1,-1):
+				self.replacement_row(i,row,col)	
 
 	def run(self):
 		self.do_forward_phase()
 		self.do_backward_phase()
 
-# Getting input
-# rows,cols = list(map(int, input("Please enter number of rows and columns respectively:\n> ").split(" ")))
-# aug_matrix = np.zeros((rows,cols))
-# for i in range(rows):
-# 	row = list(map(float, input(f"Please enter row {i+1}:\n> ").split(" ")))
-# 	for j in range (len(row)):
-# 		aug_matrix[i][j] = row[j]
+	def has_answer(self):
+		for pivot in self.pivot_positions:
+			if pivot[1] == self.cols_num-1:
+				return False
+		return True
 
-# constants = list(map(int, input("Please enter constant values:\n> ").split(" ")))
-# aug_matrix=np.hstack((aug_matrix, np.array(constants).reshape(-1,1)))
-aug_matrix = np.matrix([[0,3,-6,6,4,-5],
-							[3,-7,8,-5,8,9],
-							[3,-9,12,-9,6,15]],dtype=float)
+	def show_result(self):
+		if not self.has_answer():
+			print("The system has no answer!")
+		else:
+			for i in range(self.cols_num-1):
+				if i<self.rows_num and self.aug_matrix[i,i]==1:
+					asnwer=""
+					other_cols = [j for j in range(self.cols_num-1) if j!=i]
+					print(f"X{i+1} = {self.aug_matrix[i,-1]}",end="")
+					for j in other_cols:
+						cof = self.aug_matrix[i,j]
+						if cof>0:
+							asnwer += f" - {cof}X{j+1}"
+						elif cof<0:
+							asnwer += f" + {abs(cof)}X{j+1}"
+				else:
+					print(f"X{i+1} is a free variable")
+
+# Getting input
+rows,cols = list(map(int, input("Please enter number of rows and columns respectively:\n> ").split(" ")))
+aug_matrix = np.zeros((rows,cols))
+for i in range(rows):
+	row = list(map(float, input(f"Please enter row {i+1}:\n> ").split(" ")))
+	for j in range (len(row)):
+		aug_matrix[i][j] = row[j]
+constants = list(map(int, input("Please enter constant values:\n> ").split(" ")))
+aug_matrix=np.hstack((aug_matrix, np.array(constants).reshape(-1,1)))
+# Run algorithm
 handler = RowReductionHandler(aug_matrix)
 handler.run()
 print(handler.aug_matrix)
+handler.show_result()
